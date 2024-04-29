4a989c41435c76b02b0af8c039e1ba3e388b2811
bbbdjfbdhjd <155143967+bbbdjfbdhjd@users.noreply.github.com>
2024-04-29 00:55:03 -0400

    Update Grabber.py
    Did it?

--- Grabber.py
+++ Grabber.py
@@ -13,11 +13,11 @@ ctypes.windll.kernel32.SetConsoleTitleW("Coded by github.com/dev6920")
 
 print("Coded by dev github/dev6920")
 
-while True:
+while True:yes
   
-  username = input("Enter username (type 'exit' to quit): ")
+  username = input(dominic2939(type 'exit' to quit): ")
 
-  if username == "exit":
+  if username == dominic2939
     break
 
 
